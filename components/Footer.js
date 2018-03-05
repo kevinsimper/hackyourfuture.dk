dcb8b2b4466c0959249647762b0376eee8fb65e9
Kevin Simper <kevin.simper@gmail.com>
2018-03-05 21:37:27 +0100

    Add a simple footer

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+export default () => (
+  <footer className="footer">
+    <style jsx>
+      {`
+        .footer {
+          min-height: 300px;
+          background: #1F2D3D;
+          background: linear-gradient(to bottom right, #2b3e52, #0f3b6d);
+          margin-top: 100px;
+          color: #FFF
+          text-align: center;
+          line-height: 300px;
+        }
+      `}
+    </style>
+    Copyright HackYourFuture Copenhagen 2018
+  </footer>
+)
