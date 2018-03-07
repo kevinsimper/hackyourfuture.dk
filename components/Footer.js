48e4f0183a43f28b42414607736a067db961b47a
Kevin Simper <kevin.simper@gmail.com>
2018-03-07 23:12:01 +0100

    Show the sponsors in the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,27 @@
+import Sponsers from './Sponsers'
+
 export default () => (
   <footer className="footer">
     <style jsx>
       {`
         .footer {
+          margin-top: 100px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+        }
+        .links {
+          width: 100%;
           min-height: 300px;
           background: #1F2D3D;
           background: linear-gradient(to bottom right, #2b3e52, #0f3b6d);
-          margin-top: 100px;
           color: #FFF
           text-align: center;
           line-height: 300px;
         }
       `}
     </style>
-    Copyright HackYourFuture Copenhagen 2018
+    <Sponsers />
+    <div className="links">Copyright HackYourFuture Copenhagen 2018</div>
   </footer>
 )
