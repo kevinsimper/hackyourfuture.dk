6f8a815b23d1e0d0c7259931e536ea70bb6f6150
Kevin Simper <kevin.simper@gmail.com>
2018-05-14 22:22:37 +0200

    Add production api hostname

--- pages/login.js
+++ pages/login.js
@@ -4,7 +4,7 @@ import Content from '../components/Content'
 
 const getApi = () => {
   if(window.location.hostname === 'hackyourfuture.dk') {
-    return 'http://example.com'
+    return 'https://tdla96bnzi.execute-api.eu-central-1.amazonaws.com/production'
   } else {
     return 'http://localhost:3001'
   }
