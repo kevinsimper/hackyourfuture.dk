983174d24052c695ebc79e27da2a8fe44acf25ac
Kevin Simper <kevin.simper@gmail.com>
2018-05-14 22:45:36 +0200

    Dashboard for loggedin users

---
+++ pages/dashboard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import Content from '../components/Content'
+import { getApi } from '../services/api'
+
+export default class Dashboard extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      user: {},
+      loading: true
+    }
+  }
+  componentDidMount() {
+    console.log(getApi())
+    fetch(`${getApi()}/graphql`, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + window.localStorage.ACCESS_TOKEN
+      },
+      method: 'POST'
+    }).then(res => res.json()).then(body => {
+      this.setState({
+        user: body,
+        loading: false
+      })
+    })
+  }
+  render() {
+    return (
+      <Layout>
+        <Content>
+          <h1>Dashboard</h1>
+          {this.state.loading ? 'Loading' : 'Hi ' + this.state.user.email}
+        </Content>
+      </Layout>
+    )
+  }
+}
